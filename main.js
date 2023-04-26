d8700bacc9f1b65bec8ca4033ab02a150eabb962
nanarama <annasampers@gmail.com>
2023-04-26 11:48:45 -0500

    Get webpack running on watch

---
+++ main.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("Hello, your webpack works correctly.")
+console.log("Does it update?")
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0EsOEIiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9yZXN0YXVyYW50LXBhZ2UvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coXCJIZWxsbywgeW91ciB3ZWJwYWNrIHdvcmtzIGNvcnJlY3RseS5cIilcbmNvbnNvbGUubG9nKFwiRG9lcyBpdCB1cGRhdGU/XCIpIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9

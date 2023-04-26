f94eca0b399d2bda4a0156cc088ef46ff59cf4c3
nanarama <annasampers@gmail.com>
2023-04-26 14:35:22 -0500

    Get initial header set up

---
+++ src/header.js
@@ -0,0 +1,28 @@
+const header = () => {
+  const header = document.createElement('nav');
+  
+  const welcome = document.createElement('button');
+  welcome.innerHTML = 'Welcome';
+  welcome.id = 'welcome';
+
+  const about = document.createElement('button');
+  about.innerHTML = 'About';
+  about.id = 'about';
+
+  const menu = document.createElement('button');
+  menu.innerHTML = 'Menu';
+  menu.id = 'menu';
+
+  const contact = document.createElement('button');
+  contact.innerHTML = 'Contact';
+  contact.id = 'contact';
+
+  header.appendChild(welcome);
+  header.appendChild(about);
+  header.appendChild(menu);
+  header.appendChild(contact);
+  
+  return header;
+ };
+
+ export default header;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import './style.css';
+import header from './header';
+ 
 
- function component() {
-   const element = document.querySelector('#content');
-
-  element.innerHTML = "This is a restaurant page!";
-  element.classList.add('hello');
-
-   return element;
- }
-
- component();
+ const content = document.querySelector('#content');
+ content.appendChild(header());

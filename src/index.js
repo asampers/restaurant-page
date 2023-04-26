0d615cb192589f03b497bc24700d2c289238018f
nanarama <annasampers@gmail.com>
2023-04-26 11:54:41 -0500

    Add css loader

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-console.log("Hello, your webpack works correctly.")
-console.log("Does it update?")
+import './style.css';
+
+ function component() {
+   const element = document.createElement('div');
+
+  element.innerHTML = "This is a restaurant page!";
+  element.classList.add('hello');
+
+   return element;
+ }
+
+ document.body.appendChild(component());
